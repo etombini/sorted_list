1392ecd905a09bc71429becc084bb58fe04b23c0
Elvis <elvis@mapom.me>
2015-12-27 19:05:39 +0100

    Code modification according to memory (de-)allocation in macro

--- test/test01.c
+++ test/test01.c
@@ -1,12 +1,10 @@
-
-
 #include "slist.h"
 
 #include <stdlib.h> //arc4random
 
-slist(int); // provide a struct slist_int;
+slist(int); // provide struct slist_int;
 
-
+// comparison function
 int int_cmp(int * a, int *b)
 {
     if(*a == *b)
@@ -19,8 +17,6 @@ int int_cmp(int * a, int *b)
 
 bool test_at()
 {
-    struct slist_int * my_slist_int;
-    my_slist_int = malloc(sizeof(*my_slist_int));
     slist_init(int, my_slist_int, int_cmp);    
     bool res = true;
 
@@ -60,24 +56,13 @@ bool test_at()
 
     // Cleaning the mess
     // list_int[i] are freed in the for loop
-
-    int * garbage;
-    for(int i = 0; i < 10; i ++)
-    {
-        garbage = my_slist_int->remove_at(my_slist_int, 0);
-        free(garbage);
-    }
-    free(my_slist_int);
+    slist_free(int, my_slist_int);
 
     return true;
 }
 
-
-
 bool test_remove_at()
 {
-    struct slist_int * my_slist_int;
-    my_slist_int = malloc(sizeof(*my_slist_int));
     slist_init(int, my_slist_int, int_cmp);    
 
     int * tmp1;
@@ -93,6 +78,7 @@ bool test_remove_at()
         my_slist_int->insert(my_slist_int, v);          
     }
 
+    //TODO: why is it (size - 1) again ?
     index = arc4random_uniform(size - 1);
     tmp1 = my_slist_int->at(my_slist_int, index);
     my_slist_int->remove_at(my_slist_int, index);
@@ -109,22 +95,13 @@ bool test_remove_at()
  
     // Cleaning the mess
     free(tmp1);
+    slist_free(int, my_slist_int); 
  
-     int * garbage;
-     for(int i = 0; i < size - 1; i ++)
-     {
-         garbage = my_slist_int->remove_at(my_slist_int, 0);
-         free(garbage);
-     }
-     free(my_slist_int);
- 
-     return res;
+    return res;
 }
 
 bool test_insert()
 {
-    struct slist_int * my_slist_int;
-    my_slist_int = malloc(sizeof(*my_slist_int));
     slist_init(int, my_slist_int, int_cmp);    
 
     unsigned int size = arc4random_uniform(10000);
@@ -136,7 +113,7 @@ bool test_insert()
         my_slist_int->insert(my_slist_int, v);          
     }
 
-    for(int i = 0; i < my_slist_int->size -1 ; i++)
+    for(int i = 0; i < my_slist_int->size - 1 ; i++)
     {
         if(my_slist_int->cmp(my_slist_int->at(my_slist_int, i), my_slist_int->at(my_slist_int, i+1)) > 0)
         {
@@ -146,17 +123,10 @@ bool test_insert()
     }
 
     // Cleaning the mess
-    int * garbage;
-    for(int i = 0; i < size; i ++)
-    {
-        garbage = my_slist_int->remove_at(my_slist_int, 0);
-        free(garbage);
-    }
-    free(my_slist_int);
+    slist_free(int, my_slist_int);
+
     return true;
 }
-
-
 
 int main(int argc, char ** argv)
 {
@@ -174,45 +144,4 @@ int main(int argc, char ** argv)
         printf("[OK] test_insert succeed\n");
     else
         printf("[KO] test_inssert failed\n");
-
-    struct slist_int * my_slist_int;
-    my_slist_int = malloc(sizeof(*my_slist_int));
-    slist_init(int, my_slist_int, int_cmp);    
-
-    int * list_int[10];
-    for(int i = 0; i<10; i++)
-        list_int[i] = malloc(sizeof(list_int[i]));
-
-    printf("02\n");
-
-    * list_int[0] = 4;
-    * list_int[1] = 5;
-    * list_int[2] = 3;
-    * list_int[3] = 8;
-    * list_int[4] = 9;
-    * list_int[5] = 7;
-    * list_int[6] = 2;
-    * list_int[7] = 1;
-    * list_int[8] = 0;
-    * list_int[9] = 6;
-    
-    printf("03\n");
-
-    for(int i = 0; i<10; i++)
-        my_slist_int->insert(my_slist_int, list_int[i]);
-
-    printf("04\n");
-
-    for(int i = 0; i<my_slist_int->size; i++)
-        printf("%d: %d\n", i, * my_slist_int->at(my_slist_int, i));
-
-    printf("05\n");
-
-    my_slist_int->remove_at(my_slist_int, 5);
-
-    printf("05\n");
-    for(int i = 0; i<my_slist_int->size; i++)
-        printf("%d: %d\n", i, * my_slist_int->at(my_slist_int, i));
-
-
 }
