23c1563713748a7564a9264b43831a1e5765c566
Elvis <elvis@mapom.me>
2015-12-31 18:55:33 +0100

    adding 1 test to test_is_in

--- test/test_is_in.c
+++ test/test_is_in.c
@@ -56,10 +56,43 @@ bool test_is_in_01()
     return res;
 }
 
+bool test_is_in_02()
+{
+    slist_init(int, my_slist_int, int_cmp);    
+    struct slist_int_pos pos;
+    bool res = true;
+    unsigned int size = arc4random_uniform(10000);
+    int canary = size;
+
+    for(int i = 0; i < size; i++)
+    {
+        int * v = malloc(sizeof(*v));
+        *v = i;
+        my_slist_int->insert(my_slist_int, v);          
+    }
+
+    pos = my_slist_int->is_in(my_slist_int, &canary);
+
+    if(pos.is_in == true)
+    {
+        fprintf(stderr, "DEBUG - test_is_in - Canary %d found in slist of size %d\n", canary, size);
+        res = false;
+    }
+
+    // Cleaning the mess
+    slist_free(int, my_slist_int);
+
+    return res;
+}
+
 int main(int argc, char ** argv)
 {
     if(test_is_in_01())
         printf("[OK] test_is_in_01 succeed\n");
     else
         printf("[KO] test_is_in_01 failed\n");
+    if(test_is_in_02())
+        printf("[OK] test_is_in_02 succeed\n");
+    else
+        printf("[KO] test_is_in_02 failed\n");
 }
